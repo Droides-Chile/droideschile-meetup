dd3e153ac011e214880a1805acb273331f3a64ed
Armando Picón <picondev@gmail.com>
2023-10-31 22:04:33 -0300

    update displayPastMeetups

--- res/js/displayMeetups.js
+++ res/js/displayMeetups.js
@@ -43,55 +43,46 @@ function displayUpcomingMeetup(meetup) {
 }
 
 function displayPastMeetups(meetups) {
-    // Encuentra el contenedor donde se mostrarán los meetups pasados.
     const pastMeetupsContainer = document.getElementById('past-meetups');
-
-    // Limpia cualquier contenido previo en el contenedor.
     pastMeetupsContainer.innerHTML = '';
 
-    // Itera sobre cada meetup pasado.
-    meetups.forEach((meetup, index) => {
-        // Crea los elementos HTML para mostrar los detalles del meetup.
+    meetups.forEach(meetup => {
         const meetupElement = document.createElement('div');
         meetupElement.className = 'past-meetup';
 
-        // Crea un contenedor para el título, que actuará como toggle.
-        const titleContainer = document.createElement('div');
-        titleContainer.className = 'past-meetup-title';
-        titleContainer.innerText = meetup.title;
-        titleContainer.onclick = () => {
-            const details = document.getElementById(`meetup-details-${index}`);
-            details.style.display = details.style.display === 'none' ? 'block' : 'none';
-        };
+        const meetupTitleElement = document.createElement('div');
+        meetupTitleElement.className = 'past-meetup-title';
+        meetupTitleElement.innerText = meetup.title;
+        meetupTitleElement.addEventListener('click', () => {
+            meetupDetailsElement.classList.toggle('hidden');
+        });
 
-        // Crea un contenedor para los detalles, inicialmente oculto.
-        const detailsContainer = document.createElement('div');
-        detailsContainer.id = `meetup-details-${index}`;
-        detailsContainer.className = 'past-meetup-details';
-        detailsContainer.style.display = 'none';
+        const meetupDetailsElement = document.createElement('div');
+        meetupDetailsElement.className = 'past-meetup-details hidden';
 
-        // Agrega los detalles del meetup.
-        detailsContainer.innerHTML = `
-            <p>${meetup.date}</p>
-            <p>${meetup.location}</p>
+        meetupDetailsElement.innerHTML = `
+            <p><strong>Fecha:</strong> ${meetup.date}</p>
+            <p><strong>Lugar:</strong> ${meetup.location}</p>
         `;
 
-        // Agrega los speakers.
         const speakersList = document.createElement('ul');
         meetup.speakers.forEach(speaker => {
             const speakerItem = document.createElement('li');
-            speakerItem.innerHTML = `${speaker.name} - <a href="${speaker.linkedin}" target="_blank">LinkedIn</a>`;
+            speakerItem.innerHTML = `
+                ${speaker.name} - ${speaker.talkTitle}
+                <a href="${speaker.linkedin}" target="_blank" class="linkedin-icon">
+                    <i class="fab fa-linkedin"></i>
+                </a>
+            `;
             speakersList.appendChild(speakerItem);
         });
 
-        detailsContainer.appendChild(speakersList);
+        meetupDetailsElement.appendChild(speakersList);
 
-        // Agrega el título y los detalles al elemento del meetup.
-        meetupElement.appendChild(titleContainer);
-        meetupElement.appendChild(detailsContainer);
-
-        // Agrega el elemento del meetup al contenedor.
+        meetupElement.appendChild(meetupTitleElement);
+        meetupElement.appendChild(meetupDetailsElement);
         pastMeetupsContainer.appendChild(meetupElement);
     });
 }
+
 
